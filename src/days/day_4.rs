7a9f11ae2a537fec54b224fb5355e6b52fdc288a
adam-bates <adam@adambates.ca>
2022-12-05 10:22:48 -0500

    Day 4.

--- src/days/day_4.rs
+++ src/days/day_4.rs
@@ -7,17 +7,71 @@ const INPUT_FILE: &'static str = "input_4.txt";
 pub fn part_1() -> Result<String> {
     let input = utils::read_input_file(INPUT_FILE)?;
 
-    let res = "TODO";
+    let mut fully_contained = 0;
 
-    return Ok(res.to_string());
+    for line in input.lines() {
+        let (range_1, range_2) = line.split_once(",").unwrap();
+
+        let (range_1_min, range_1_max) = range_1.split_once("-").unwrap();
+        let (range_2_min, range_2_max) = range_2.split_once("-").unwrap();
+
+        let (range_1_min, range_1_max) = (
+            range_1_min.parse::<usize>().unwrap(),
+            range_1_max.parse::<usize>().unwrap(),
+        );
+
+        let (range_2_min, range_2_max) = (
+            range_2_min.parse::<usize>().unwrap(),
+            range_2_max.parse::<usize>().unwrap(),
+        );
+
+        if (
+            range_1_min <= range_2_min && range_1_max >= range_2_max
+        ) || (
+            range_2_min <= range_1_min && range_2_max >= range_1_max
+        ) {
+            fully_contained += 1;
+        }
+    }
+
+    return Ok(fully_contained.to_string());
 }
 
 pub fn part_2() -> Result<String> {
     let input = utils::read_input_file(INPUT_FILE)?;
 
-    let res = "TODO";
+    let mut overlapping = 0;
 
-    return Ok(res.to_string());
+    for line in input.lines() {
+        let (range_1, range_2) = line.split_once(",").unwrap();
+
+        let (range_1_min, range_1_max) = range_1.split_once("-").unwrap();
+        let (range_2_min, range_2_max) = range_2.split_once("-").unwrap();
+
+        let (range_1_min, range_1_max) = (
+            range_1_min.parse::<usize>().unwrap(),
+            range_1_max.parse::<usize>().unwrap(),
+        );
+
+        let (range_2_min, range_2_max) = (
+            range_2_min.parse::<usize>().unwrap(),
+            range_2_max.parse::<usize>().unwrap(),
+        );
+
+        if (
+            range_1_min <= range_2_min && range_2_min <= range_1_max
+        ) || (
+            range_1_min <= range_2_max && range_2_max <= range_1_max
+        ) || (
+            range_2_min <= range_1_min && range_1_min <= range_2_max
+        ) || (
+            range_2_min <= range_1_max && range_1_max <= range_2_max
+        ) {
+            overlapping += 1;
+        }
+    }
+
+    return Ok(overlapping.to_string());
 }
 
 
